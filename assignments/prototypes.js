2b459456605af3a12e2ca3db82da18532912167e
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-10-17 15:09:31 +0100

    Add GameObject

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,24 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject (attr) {
+  this.createdAt = attr.createdAt;
+  this.name = attr.name;
+  this.dimensions = attr.dimensions;
+}
+
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game.`
+}
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
